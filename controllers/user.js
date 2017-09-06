4c94c39c33462c62610c0431f77fa6ebd8393ce5
jwats287 <jwats287@users.noreply.github.com>
2017-09-05 20:04:51 -0400

    Reset for new npm

--- controllers/user.js
+++ controllers/user.js
@@ -2,46 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose')
 const User = require("../models/user");
-const json2csv = require('json2csv');
 
 
 
 
-var MongoClient = require('mongodb').MongoClient, assert = require('assert');
-router.get('/csv', function (req, res) {
-  console.log("Hit the CSV route");
-    // var url = 'mongodb://admin:detroit123@ds063946.mlab.com:63946/misale_dev';
-    
-    //
-    // This function should be used for migrating a db table to a TBD format
-    //
-    var migrateMongoDBTable = function(db, callback) {
-        // Get the documents collection
-        console.log("Reading database records");
-        // Get the documents collection
-        var collection = db.collection('users');
-        console.log(collection)
-        // Find some documents
-        //collection.find({'a': 3}).toArray(function(err, docs) {
-        collection.find({}).toArray(function(err, docs) {
-          assert.equal(err, null);
-          console.log(docs)
-          //console.log(docs);
-          //console.log('docs.length ---> ', docs.length);
-          console.log('Creating CSV...');
-          //console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=');
-          var json2csv = require('json2csv');
-          var fields = ['_id', 'JobID', 'LastApplied'];
-          var fieldNames = ['ID_OR_PK', 'JOB_UNIQUE_ID_TITLE', 'APPLICATION_DATE'];
-          var data = json2csv({ data: docs, fields: fields, fieldNames: fieldNames });
-          //console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=');
-          // EXPORT FILE
-          res.attachment('yourfilenamehere.csv');
-          res.status(200).send(data);
-          callback(docs);
-        });
-    }
-  });
+
 
 router.get("/", (req, res) => {
   User.find().then((user) => {
